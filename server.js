4d8ad05563b29a863780ffd25d2c792ad0e0350a
Samantha Allen <samanthaamiraallen@gmail.com>
2024-01-23 15:12:59 -0400

    added middleware for parsing json and url encoded data

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
